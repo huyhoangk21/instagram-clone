1fca3ca73e6c3bee3a0c70fc81dc533a2687b4bc
Hoang Le <kuhpro@gmail.com>
2020-12-16 17:28:16 -0600

    add image storage and upload

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 <div class="container">
-    <form action="">
+    <form action="/p" enctype="multipart/form-data" method="post">
+        @csrf
         <div class="row">
             <div class="col-8 offset-2">
                 <div class="row">
@@ -26,7 +27,7 @@
                 <div class="row">
                     <label for="image" class="col-md-4 col-form-label">{{ __('Post Image') }}</label>
                     <input type="file" class="form-control-file @error('image') is-invalid @enderror" id="image" name="image">
-                    @error('caption')
+                    @error('image')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
